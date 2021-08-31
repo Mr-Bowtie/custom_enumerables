74655222274fe5f8c17b376b2b6fcd6de51f3797
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-31 18:49:41 -0500

    fix: updated test for symbol support

--- my_inject_test.rb
+++ my_inject_test.rb
@@ -6,7 +6,11 @@ puts '==: my_inject'
 p arr.my_inject() { |sum, n| sum + n }
 p arr.my_inject(3) { |sum, n| sum + n }
 p hsh.my_inject(0) { |sum, (k, v)| sum + v }
+p arr.my_inject(:+)
+p arr.my_inject(3, :*)
 puts '==: inject'
 p arr.inject() { |sum, n| sum + n }
 p arr.inject(3) { |sum, n| sum + n }
 p hsh.inject(0) { |sum, (k, v)| sum + v }
+p arr.inject(:+)
+p arr.inject(3, :*)
