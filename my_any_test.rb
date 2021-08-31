88b8f8bbb5bc17fa3117465a280341bd639ad50d
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-30 23:41:34 -0500

    fix: tests added my #my_any?

---
+++ my_any_test.rb
@@ -0,0 +1,15 @@
+require_relative 'enum_methods'
+
+arr = [1, 2, 3, 'hello', nil]
+hsh = { a: 1, b: 'hey', c: [1, 2] }
+
+puts '==: my_any?'
+puts arr.my_any?
+puts arr.my_any?(String)
+puts arr.my_any? { |el| el % 2 == 0 }
+puts hsh.my_any? { |k, v| v.instance_of?(String) }
+puts '==: any?'
+puts arr.any?
+puts arr.any?(String)
+puts arr.any? { |el| el % 2 == 0 }
+puts hsh.any? { |k, v| v.instance_of?(String) }
