d290472d9b6179422745c43fcf9ca35de505e44b
Mr-Bowtie <mr.bowtie989@gmail.com>
2021-08-31 22:54:41 -0500

    fix: added #multiply_els and tests

--- my_inject_test.rb
+++ my_inject_test.rb
@@ -1,14 +1,21 @@
 require_relative 'enum_methods'
 
+def multiply_els(arr)
+  arr.my_inject(:*)
+end
+
 arr = [1, 2, 3]
+arr2 = [2, 4, 5]
 hsh = { a: 1, b: 2, c: 3 }
-puts '==: my_inject'
+puts '==: #my_inject'
 p arr.my_inject() { |sum, n| sum + n }
 p arr.my_inject(3) { |sum, n| sum + n }
 p hsh.my_inject(0) { |sum, (k, v)| sum + v }
 p arr.my_inject(:+)
 p arr.my_inject(3, :*)
-puts '==: inject'
+puts '==: #multiply_els'
+p multiply_els(arr2)
+puts '==: #inject'
 p arr.inject() { |sum, n| sum + n }
 p arr.inject(3) { |sum, n| sum + n }
 p hsh.inject(0) { |sum, (k, v)| sum + v }
